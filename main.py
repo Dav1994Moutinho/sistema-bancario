acd9c1d89968de910f5911cd1993352057236809
David Moutinho <davidmoutinho1994@gmail.com>
2025-03-22 17:50:42 -0300

    Comite final, finalizando e adicionando data, hora e validacao de dia.

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
+from datetime import datetime, timedelta
+from zoneinfo import ZoneInfo
+
+
 menu = """
 
 [d] Depositar
@@ -15,41 +19,61 @@ LIMITE_SAQUES = 3
 
 numero_transacoes = 0
 LIMITE_TRANSACOES = 10
+data_primeira_transacao = None
+
 
 while True:
 
+### validacao de 1 dia
+  if data_primeira_transacao:
+    agora = datetime.now(ZoneInfo("America/Sao_Paulo"))
+    data_formatada = agora.strftime("%d/%m/%Y %H:%M:%S")
+
+    if agora.date() > data_primeira_transacao.date():
+      numero_transacoes = 0
+      numero_saques = 0
+      data_primeira_transacao = None
+      print("\nNovo dia detectado. Contadores de transações e saques foram reiniciados.\n")
+
   opcao = input(menu)
-
-
+    
 
 ### CÓDIGO PARA DEPOSITAR
   if opcao == "d":
     if numero_transacoes >= LIMITE_TRANSACOES:
       print ("Limite diário de transações excedido")
+      continue
 
     valor = float(input("Informe o valor do depósito: "))
 
     if valor > 0:
+      agora = datetime.now(ZoneInfo("America/Sao_Paulo"))
+      data_formatada = agora.strftime("%d/%m/%Y %H:%M:%S")
+
       saldo += valor
       extrato += f"Depósito: R$ {valor:.2f}\n"
+      extrato += f"{data_formatada}\n\n"
       numero_transacoes += 1
 
+      if not data_primeira_transacao:
+        data_primeira_transacao = agora
+      
     else:
-      print("Operação falhou! O valor informado é inválido!")
-
+      print("Valor informado é inválido!")
 
 
 # CÓDIGO PARA SACAR    
   elif opcao == "s":
+
+    if numero_transacoes >= LIMITE_TRANSACOES:
+      print("Limite diário de transações excedido.")
+      continue
+
     valor = float(input("Informe o valor do saque: "))
 
     excedeu_saldo = valor > saldo
-
     excedeu_limite = valor > limite
-
     excedeu_saques = numero_saques >= LIMITE_SAQUES
-
-    excedeu_transacoes = numero_transacoes >= LIMITE_TRANSACOES
 
     if excedeu_saldo:
       print("Seu saldo é insuficiente.")
@@ -60,14 +84,18 @@ while True:
     elif excedeu_saques:
       print("Número máximo de saques excedido.")
 
-    elif excedeu_transacoes:
-      print("Limite diário de transações excedido!")
-
     elif valor > 0:
+      agora = datetime.now(ZoneInfo("America/Sao_Paulo"))
+      data_formatada = agora.strftime("%d/%m/%Y %H:%M:%S")
+
       saldo -= valor
       extrato += f"Saque: R$ {valor:.2f}\n"
+      extrato += f"{data_formatada}\n\n"
       numero_saques += 1
+      numero_transacoes += 1
       
+      if not data_primeira_transacao:
+        data_primeira_transacao = agora
     
     else:
       print("Valor informado é inválido.")
